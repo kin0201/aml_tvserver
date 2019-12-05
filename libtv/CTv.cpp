71fc8a984a2684f46b6a53a02edadd3169c8f7fa
Song Zhao <song.zhao@amlogic.com>
2019-12-05 11:05:49 -0800

    publish license
    Change-Id: I4572f0be378e093e223f4c7538ac013118724d44

--- libtv/CTv.cpp
+++ libtv/CTv.cpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ *
+ * This source code is subject to the terms and conditions defined in the
+ * file 'LICENSE' which is part of this source code package.
+ *
+ * Description:
+ */
 #define LOG_MOUDLE_TAG "TV"
 #define LOG_CLASS_TAG "CTv"
 
